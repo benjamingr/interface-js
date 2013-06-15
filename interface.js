9da8b3aa264a35b224e07b9f83b18327eb7ff2ff
inglor <inglor@gmail.com>
2013-06-15 21:15:43 +0300

    Replaced tabs with spaced :S

--- interface.js
+++ interface.js
@@ -1,45 +1,47 @@
 function breach(message){
-	return new Error("Breach in interface contract: "+message);
+    return new Error("Breach in interface contract: "+message);
 }
 
 function Interface(fn){
-	//get object, or construct if doesn't return object
-	var iface = new fn();
+    //get object, or construct if doesn't return object
+    var iface = new fn();
 
-	return function(impl){
-		
-		// Proxy handler
-		var handler = {
-    		get: function(target, name){
-        		if(name in iface){
-		            return impl[name];
-    			}
-    			return undefined;
-    		}
-		};
+    return function(impl){
+        
+        // Proxy handler
+        var handler = {
+            get: function(target, name){
+                if(name in iface){
+                    return impl[name];
+                }
+                return undefined;
+            }
+        };
 
-		// Enforce implementation
-		for(elem in iface){
-			
-			if(!(elem in impl)){
-				throw breach(" Nonexisting member "+elem)
-			}
-			var contract = iface[elem];
-			var tested = impl[elem]
-			if(typeof contract === "function"){
-				if(typeof tested !== "function"){
-					throw breach(" Interface function "+elem+" not a function in implementor")
-				}
-				if(contract.length > tested.length){
-					throw breach(" Interface required function "+elem+" to run with "+contract.length+" params"+
-								 " however implementor's runs with "+tested.length+" params")
-				}
-			}
-		}
+        // Enforce implementation
+        for(elem in iface){
+            
+            if(!(elem in impl)){
+                throw breach(" Nonexisting member "+elem)
+            }
+            var contract = iface[elem];
+            var tested = impl[elem]
+            if(typeof contract === "function"){
+                if(typeof tested !== "function"){
+                    throw breach(" Interface function "+elem+" not a function in implementor")
+                }
+                if(contract.length > tested.length){
+                    throw breach(" Interface required function "+elem+" to run with "+
+                                 contract.length+" params however implementor's runs"+
+                                 " with "+tested.length+" params")
+                }
+            }
 
-		var p = Proxy.create(handler);
-		return p;
-	};
+        }
+
+        var p = Proxy.create(handler);
+        return p;
+    };
 }
 
 module.exports = Interface;
